1c12db385435527b1250c6155300136bed8b005f
thuanvo <thuan@wearebloom.com>
2023-11-30 08:21:07 +0700

    update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://thuonghanluan.vercel.app/api/',
 })
 
 api.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
